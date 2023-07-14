1abcfb9921339fdeee23159cd3cc1450ddbd4b01
sarvanshiriev <sarvan.shiriev@mail.ru>
2023-07-14 19:18:17 +0500

    Сделаны изменения во всем проекте,слайдер,стили,наполнение

--- js/main.js
+++ js/main.js
@@ -42,3 +42,16 @@ btnDarkMode.onclick = function () {
   }
 };
 
+//Слайдер 
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
