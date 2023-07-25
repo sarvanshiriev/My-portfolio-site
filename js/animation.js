f5b56031b869ec08f2287ce800dc5794fec8820f
Sarvan Shiriev <sarvan.shiriev@mail.ru>
2023-07-25 16:13:20 +0500

    Исправлена анимация печатания

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,16 @@
+if (window.innerWidth < 768) {
+    const elements = document.querySelectorAll('.header__description');
+    elements.forEach(element => {
+      element.classList.add('animate__animated');
+      element.classList.add('animate__wobble');
+    });
+}
+
 function startAnimation() {
     document.querySelector('.header__description').style.visibility = 'visible';
 }
 
 setTimeout(startAnimation, 4100);
+
+
+
